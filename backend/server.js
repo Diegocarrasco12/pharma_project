4b1b0702e4aa99de7030cd8e25394864673971a9
Diego_Carrasco <diegocarrasco.ordonez@gmail.com>
2025-06-14 19:24:21 -0400

    Commit seguro ignorando .env

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import pool from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import authRoutes from './routes/authRoutes.js';
-import userRoutes from './routes/userRoutes.js'; // ✅ requiere export default
+import userRoutes from './routes/userRoutes.js'; 
 import contactRoutes from './routes/contactRoutes.js';
 
 dotenv.config();
@@ -23,6 +23,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+// Ruta raíz para Render
+app.get('/', (req, res) => {
+  res.send('Servidor backend activo');
+});
+
 // Ruta de prueba
 app.get('/api', (req, res) => {
   res.json({ message: '¡API de Pharma Project funcionando correctamente!' });
@@ -37,13 +42,11 @@ pool.connect()
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes); // ✅ Correcta si userRoutes.js usa export default
+app.use('/api/users', userRoutes); 
 app.use('/api/contact', contactRoutes);
 
 // Iniciar servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-
-
+  console.log(`✅ Servidor escuchando en puerto ${PORT}`);
 });
