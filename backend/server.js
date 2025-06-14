00b2414785b9ce31bc3208546d2ea42b17b68a8a
Diego_Carrasco <diegocarrasco.ordonez@gmail.com>
2025-06-14 19:19:19 -0400

    Commit seguro ignorando .env

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,7 @@ app.use('/api/contact', contactRoutes);
 // Iniciar servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
+
 
 });
